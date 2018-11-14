d528926a7a1f52d4aecaa7bb3d0823ea560ed93e
Julian McNeill <julesdowork@gmail.com>
2018-11-13 21:45:49 -0500

    Integration 1 - Player Model

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+    const float locomotionAnimationSmoothTime = .1f;
+
+    private Animator anim;
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        anim.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+	}
+}
