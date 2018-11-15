9e39947ad3b70052e307dc9e4c324bf8a04ba12b
Julian McNeill <julesdowork@gmail.com>
2018-11-14 19:42:38 -0500

    Equipment

---
+++ Assets/Scripts/Items/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield, Feet }
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item {
+
+    public EquipmentSlot equipSlot;
+
+    public int armorModifier;
+    public int damageModifier;
+
+    public override void Use()
+    {
+        base.Use();
+
+        // Equip the item
+        EquipmentManager.instance.Equip(this);
+
+        // Remove it from the inventory
+        RemoveFromInventory();
+    }
+}
